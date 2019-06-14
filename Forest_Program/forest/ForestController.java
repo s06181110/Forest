7885cd18b8807b9225179df1a266abafbb65aad5
wolf <nyan643.ym25mm10@gmail.com>
2019-06-15 02:45:57 +0900

    fixed ForestController & add method set*

--- Forest_Program/forest/ForestController.java
+++ Forest_Program/forest/ForestController.java
@@ -1,6 +1,8 @@
 package forest;
 
 import mvc.Controller;
+
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 
 /**
@@ -13,8 +15,39 @@ public class ForestController extends Controller {
 	 *  このクラスのインスタンスを生成するコンストラクタです。
 	 * 
 	 */
-	public ForestController() { //construct for Instance
-		ForestController forestController = new ForestController();
+
+	/**
+	 * 情報を握っているModelのインスタンスを束縛する。
+	 */
+	protected Model model;
+
+	/**
+	 * 表示を司るViewのインスタンスを束縛する。
+	 */
+	protected View view;
+
+	/**
+	 * 以前にマウスのボタンが押下された場所をPointのインスタンスとして束縛する。
+	 */
+	private Point previous;
+
+	/**
+	 * 現在にマウスのボタンが押下された場所をPointのインスタンスとして束縛する。
+	 */
+	private Point current;
+
+	/**
+	 * インスタンスを生成して応答する。
+	 * すべてのインスタンス変数（model, view, previous, current）をnull化する。
+	 */
+	public ForestController()
+	{
+		super();
+		this.model = null;
+		this.view = null;
+		this.previous = null;
+		this.current = null;
+		return;
 	}
 
 	/**
@@ -24,7 +57,34 @@ public class ForestController extends Controller {
 	 * 
 	 */
 	public void mouseClicked(MouseEvent aMouseEvent) { //return:void
-		
+		Point aPoint = aMouseEvent.getPoint();
+		aPoint.translate(view.scrollAmount().x, view.scrollAmount().y);
+		System.out.println(aPoint);
+		return;
+	}
+	
+	/**
+	 * 指定されたモデルをインスタンス変数modelに設定する。
+	 * @param aModel このコントローラのモデル
+	 */
+	public void setModel(Model aModel)
+	{
+		this.model = aModel;
+		return;
+	}
+
+	/**
+	 * 指定されたビューをインスタンス変数viewに設定し、
+	 * ビューのマウスのリスナおよびモーションリスナそしてホイールリスナをこのコントローラにする。
+	 * @param aView このコントローラのビュー
+	 */
+	public void setView(View aView)
+	{
+		this.view = aView;
+		this.view.addMouseListener(this);
+		this.view.addMouseMotionListener(this);
+		this.view.addMouseWheelListener(this);
+		return;
 	}
 
 }
